48a1d58958e8998397f974f6531fab7b2ec2044c
akbokha <a.k.bokharouss@student.tue.nl>
2016-05-26 23:34:45 +0200

    Test 2

--- app/src/main/java/com/thermostatmobileak/android/mobilethermostat/MainActivity.java
+++ app/src/main/java/com/thermostatmobileak/android/mobilethermostat/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Github commit and push test - akbokha
+    // Github commit and push test 2 - akbokha
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
